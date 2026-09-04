1370bf9317ab34f4693f5eba9715e1aeff708d5b
Pratik Bairoliya <pratik0520@gmail.com>
2026-09-04 09:00:04 -0400

    leetcode: sync 2026-09-04

---
+++ Medium/0131-palindrome-partitioning/solution.py
@@ -0,0 +1,10 @@
+"""131. Palindrome Partitioning (Medium)
+
+https://leetcode.com/problems/palindrome-partitioning/
+"""
+from typing import List, Optional  # noqa: F401
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        pass
